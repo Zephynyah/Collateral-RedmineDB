ff6163956990f64b6e5318acf28fc81d1d4d046a
Zephynyah <jah_hick@yahoo.com>
2025-09-09 20:58:39 -0400

    Refactor verbosity settings in PowerShell scripts and update .gitignore to include 'Old/' directory

--- Collateral-RedmineDB.ps1
+++ Collateral-RedmineDB.ps1
@@ -1,4 +1,4 @@
-
+$VerbosePreference = "Continue"
 
 Import-Module .\Collateral-RedmineDB.psm1 -Force -Verbose
 
@@ -7,5 +7,8 @@ $key = "c5fc2de08e46b51bbd8c0a448c0b08f35e99004d"
 Connect-Redmine -Server "http://localhost:8080" -Key $key
 
 # Connect-Redmine -Server "http://localhost:8080" -Username "admin"
+
+
+$VerbosePreference = "silentlyContinue"
 
 
